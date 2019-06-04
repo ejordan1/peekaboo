8d9abc9416da71a17dd129a1e8d516b5010d175c
Emerson Jordan and saswati Patra <saswati@>
2019-06-04 09:41:47 -0700

    updated with fade-in, fade-out, slide-in and slide-in

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickable0").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+
+
+  });
+
+  $(".clickable1").click(function() {
+
+    $("#walrus-fadeout").fadeToggle();
+    $("#walrus-fadein").fadeToggle();
+
+
+  });
+
+  $(".clickable2").click(function() {
+
+    $("#walrus-slideout").slideToggle();
+    $("#walrus-slidein").slideToggle();
+
+
+  });
+
+  $(".clickable3").click(function() {
+
+    $("#peekabooshow").slideToggle();
+    $("#peekaboohidden").slideToggle();
   });
 });
