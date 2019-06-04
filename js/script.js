9c0f63fadd16cb0b32ed199dfe0e47987e04fb9c
Emerson Jordan and saswati Patra <saswati@>
2019-06-04 11:27:04 -0700

    updated with buttons and bug

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
 $(document).ready(function() {
+
+  $("button#dark").click(function() {
+      // $("body").removeClass();
+      $("body").addClass("dark-background");
+
+      $("body").addClass("buttonsBecomeDark");
+      // $("body").addClass("inside-dark");
+
+    });
+
+    $("button#light").click(function() {
+        $("body").removeClass();
+        $("body").addClass("light-background");
+        // $("body").addClass("inside-light");
+
+      });
+
+  $("button#green").click(function() {
+      $("body").removeClass();
+      $("body").addClass("green-background");
+
+    });
+
+    $("button#yellow").click(function() {
+      $("body").removeClass();
+      $("body").addClass("yellow-background");
+
+    });
+
+    $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+
+    });
+    $("button#white").click(function() {
+      $("body").removeClass();
+
+    });
+
+
   $(".clickable0").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
@@ -23,7 +63,7 @@ $(document).ready(function() {
   });
 
   $(".clickable3").click(function() {
-
+    $("body").removeClass();
     $("#peekabooshow").slideToggle();
     $("#peekaboohidden").slideToggle();
   });
